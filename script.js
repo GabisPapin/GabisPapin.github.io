98802bf0e177873939e8fabe09bbb857bf530f4a
Gabriela Papin <gabrielapapin20@gmail.com>
2022-05-30 22:41:17 -0300

    arquivos duplicados excluidos

--- script.js
+++
@@ -1,9 +0,0 @@
-const botaoFoto = document.getElementById("botaoFoto");
-const botaoHabil = document.getElementById("botaoHabil");
-const botaoHobbies = document.getElementById("botaoHobbies");
-
-botaoFoto.addEventListener('click', function(){
-    let localFoto = document.getElementById("foto");
-    localFoto.scrollIntoView({block: "center", behavior: "auto"});
-    //não funcionou, testar outras formas.
-})
